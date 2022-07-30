3a0c4db08ebe62b65bd1a8e6ff3930cf81aadcbc
xSupFC <109413786+xSupFC@users.noreply.github.com>
2022-07-30 13:10:42 +0800

    Update and rename Venyx to Venyx.lua

--- Venyx.lua
+++ Venyx.lua
@@ -221,7 +221,7 @@ do
 				Name = "Main",
 				BackgroundTransparency = 1,
 				Position = UDim2.new(0.25, 0, 0.052435593, 0),
-				Size = UDim2.new(0, 511, 0, 428),
+				Size = UDim2.new(0, 450, 0, 428),
 				Image = "rbxassetid://4641149554",
 				ImageColor3 = themes.Background,
 				ScaleType = Enum.ScaleType.Slice,
