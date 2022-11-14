69c4eecbca910649548cfbd8b28db933b4f1edb8
xSupFC <109413786+xSupFC@users.noreply.github.com>
2022-11-14 18:54:28 +0800

    Update and rename RayfieldSmall.lua to Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -1247,10 +1247,10 @@ function RayfieldLibrary:CreateWindow(Settings)
 				end
 			end
 			if Elements.UIPageLayout.CurrentPage ~= TabPage then
-				TweenService:Create(Elements, TweenInfo.new(1, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 460,0, 330)}):Play()
+				TweenService:Create(Elements, TweenInfo.new(1, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 390,0, 330)}):Play()
 				Elements.UIPageLayout:JumpTo(TabPage)
 				wait(0.2)
-				TweenService:Create(Elements, TweenInfo.new(0.8, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 475,0, 366)}):Play()
+				TweenService:Create(Elements, TweenInfo.new(0.8, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 400,0, 366)}):Play()
 			end
 
 		end)
